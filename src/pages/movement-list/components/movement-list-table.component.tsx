0d4181a872ae00e4eb2e8c2901b452dadb0e6030
Camila Villalba <cami_vp_23@hotmail.com>
2024-11-12 14:20:45 +0100

    finishing movement list item component

--- src/pages/movement-list/components/movement-list-table.component.tsx
+++ src/pages/movement-list/components/movement-list-table.component.tsx
@@ -1,28 +1,34 @@
 import React from "react";
-import {MovementVm} from "../movement-list.vm";
+import { MovementVm } from "../movement-list.vm";
 import classes from "./movement-list-table.component.module.css";
 interface Props {
-    movementList: MovementVm[];
+  movementList: MovementVm[];
 }
+import { MovementListItemComponent } from "./movement-list-item.component";
 
-export const MovementListTableComponent : React.FC<Props> = (props) => {
-    const {movementList} = props;
+export const MovementListTableComponent: React.FC<Props> = (props) => {
+  const { movementList } = props;
 
-    return (
-        <>
-        <div className={classes.gridContainer}>
-            <div className={classes.gridTable}>
-                <div className={classes.headerTable}>
-                    <span className={classes.headerCell}>FECHA</span>
-                    <span className={classes.headerCell}>FECHA VALOR</span>
-                    <span className={classes.headerCell}>DESCRIPCIÓN</span>
-                    <span className={classes.headerCell}>IMPORTE</span>
-                    <span className={classes.headerCell}>SALDO DISPONIBLE</span>
-                </div>
-            </div>
+  return (
+    <>
+      <div className={classes.gridContainer}>
+        <div className={classes.gridTable}>
+          <div className={classes.headerTable}>
+            <span className={classes.headerCell}>FECHA</span>
+            <span className={classes.headerCell}>FECHA VALOR</span>
+            <span className={classes.headerCell}>DESCRIPCIÓN</span>
+            <span className={classes.headerCell}>IMPORTE</span>
+            <span className={classes.headerCell}>SALDO DISPONIBLE</span>
+          </div>
 
-
+          {movementList.map((movement) => (
+            <MovementListItemComponent
+              key={movement.accountId}
+              movementItem={movement}
+            />
+          ))}
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
--- src/pages/movement-list/movement-list.page.tsx
+++ src/pages/movement-list/movement-list.page.tsx
@@ -1,53 +1,52 @@
 import { AppLayout } from "@/layaouts";
 import React from "react";
-import {MovementVm} from "./movement-list.vm"
+import { MovementVm } from "./movement-list.vm";
 import classes from "./movement-list.page.module.css";
 import { useAccountDataContext } from "@/core/profile/account.context";
-import {MovementListTableComponent} from "./components/movement-list-table.component"
+import { MovementListTableComponent } from "./components/movement-list-table.component";
 
 const movementsMock = [
   {
-    "id": "4",
-    "description": "Luz diciembre",
-    "amount": -110,
-    "balance": 2480,
-    "transaction": new Date("2020-01-02T10:00:00"),
-    "realTransaction": new Date("2020-01-03T10:00:00"),
-    "accountId": "2"
+    id: "4",
+    description: "Luz diciembre",
+    amount: -110,
+    balance: 2480,
+    transaction: new Date("2020-01-02T10:00:00"),
+    realTransaction: new Date("2020-01-03T10:00:00"),
+    accountId: "2",
   },
   {
-    "id": "5",
-    "description": "Agua diciembre",
-    "amount": -20,
-    "balance": 2590,
-    "transaction": new Date("2020-01-01T09:00:00"),
-    "realTransaction": new Date("2020-01-01T09:00:00"),
-    "accountId": "2"
+    id: "5",
+    description: "Agua diciembre",
+    amount: -20,
+    balance: 2590,
+    transaction: new Date("2020-01-01T09:00:00"),
+    realTransaction: new Date("2020-01-01T09:00:00"),
+    accountId: "2",
   },
   {
-    "id": "6",
-    "description": "Internet diciembre",
-    "amount": -35,
-    "balance": 2610,
-    "transaction": new Date("2020-01-01T08:00:00"),
-    "realTransaction": new Date("2020-01-01T08:00:00"),
-    "accountId": "2"
+    id: "6",
+    description: "Internet diciembre",
+    amount: -35,
+    balance: 2610,
+    transaction: new Date("2020-01-01T08:00:00"),
+    realTransaction: new Date("2020-01-01T08:00:00"),
+    accountId: "2",
   },
   {
-    "id": "7",
-    "description": "Luz noviembre",
-    "amount": -89,
-    "balance": 2645,
-    "transaction": new Date("2019-12-01T07:01:00"),
-    "realTransaction": new Date ("2019-12-02T12:00:10"),
-    "accountId": "2"
-  }
-]
+    id: "7",
+    description: "Luz noviembre",
+    amount: -89,
+    balance: 2645,
+    transaction: new Date("2019-12-01T07:01:00"),
+    realTransaction: new Date("2019-12-02T12:00:10"),
+    accountId: "2",
+  },
+];
 
 export const MovementListPage: React.FC = () => {
   const [movementList] = React.useState<MovementVm[]>(movementsMock);
-  const {iban, saldo} = useAccountDataContext();
-  
+  const { iban, saldo } = useAccountDataContext();
 
   return (
     <AppLayout>
@@ -61,12 +60,9 @@ export const MovementListPage: React.FC = () => {
           <span>IBAN: {iban}</span>
         </div>
         <MovementListTableComponent
-        movementList={movementList}></MovementListTableComponent>
-
-
+          movementList={movementList}
+        ></MovementListTableComponent>
       </div>
     </AppLayout>
   );
 };
-
-
