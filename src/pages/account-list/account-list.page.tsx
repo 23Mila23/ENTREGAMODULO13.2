01ab0d6faf95f1954995949fcec23d5f8b02a011
Camila Villalba <cami_vp_23@hotmail.com>
2024-11-21 12:01:42 +0100

    added handleOnClick function to agregar nueva cuenta button

--- src/pages/account-list/account-list.page.tsx
+++ src/pages/account-list/account-list.page.tsx
@@ -5,21 +5,30 @@ import classes from "./account-list.page.module.css";
 import { AccountListTableComponent } from "./components";
 import { getAccountList } from "./api";
 import { mapAccountListFromApiToVm } from "./account-list.mapper";
+import { useNavigate } from "react-router-dom";
+import { appRoutes } from "@/core/router";
 
 export const AccountListPage: React.FC = () => {
   const [accountList, setAccountList] = React.useState<AccountVm[]>([]);
+
   React.useEffect(() => {
     getAccountList().then((result) =>
       setAccountList(mapAccountListFromApiToVm(result))
     );
   }, []);
+
+  const navigate = useNavigate();
+
+  const handleButtonOnClick = () => {
+    navigate(appRoutes.newAccount)
+  }
   
   return (
     <AppLayout>
       <div className={classes.root}>
         <div className={classes.headerContainer}>
           <h1>Mis cuentas</h1>
-          <button> AGREGAR NUEVA CUENTA</button>
+          <button onClick={handleButtonOnClick}> AGREGAR NUEVA CUENTA</button>
         </div>
         <AccountListTableComponent
           accountList={accountList}
