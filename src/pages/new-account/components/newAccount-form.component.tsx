42a5d3cc73019975ede6cfbe3c5e92bf1efd139b
Camila Villalba <cami_vp_23@hotmail.com>
2024-11-21 14:10:59 +0100

    creating new account form components select logic

--- src/pages/new-account/components/newAccount-form.component.tsx
+++ src/pages/new-account/components/newAccount-form.component.tsx
@@ -1,11 +1,35 @@
-import { Account } from "../newAccount.vm";
-import classes from "./newAccount-form.component.module.css"
+import React from "react";
+import { Account, createEmptyAccount } from "../newAccount.vm";
+import classes from "./newAccount-form.component.module.css";
 
-interface Props {
-  account: Account;
-}
-export const NewAccountFormComponent: React.FC<Props> = (props) => {
-  const { account } = props;
+const accountTypes = [
+  {
+    id: "1",
+    type: "Cuenta Corriente",
+  },
+  {
+    id: "2",
+    type: "Cuenta de Ahorro",
+  },
+  {
+    id: "3",
+    type: "Cuenta de Nómina",
+  },
+];
+
+export const NewAccountFormComponent: React.FC = () => {
+  const [account, setAccount] = React.useState<Account>(createEmptyAccount());
+
+  const handleOnChange = (
+    e:
+      | React.ChangeEvent<HTMLSelectElement>
+      | React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setAccount({
+      ...account,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div>
@@ -13,18 +37,29 @@ export const NewAccountFormComponent: React.FC<Props> = (props) => {
         <div className={classes.formContainer}>
           <div>
             <label>Tipo de cuenta: </label>
-            <select className={classes.typeSelect}>
-              <option key={account.type} value={account.type}>
-                {account.type}
-              </option>
+            <select
+              onChange={handleOnChange}
+              name="type"
+              className={classes.typeSelect}
+            >
+              <option value="">Seleccionar</option>
+              {accountTypes.map((accountType) => {
+                return (
+                  <option key={accountType.id} value={accountType.id}>
+                    {accountType.type}
+                  </option>
+                );
+              })}
             </select>
           </div>
           <div>
             <label>Alias: </label>
-            <input className={classes.alias}></input>
+            <input name="name" className={classes.alias} />
           </div>
         </div>
-        <button type="submit" className={classes.button}>GUARDAR</button>
+        <button type="submit" className={classes.button}>
+          GUARDAR
+        </button>
       </form>
     </div>
   );
