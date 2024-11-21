bd937c6b229d5edcd325be19dbb22e1bbf3f446d
Camila Villalba <cami_vp_23@hotmail.com>
2024-11-21 13:06:51 +0100

    finished css in newAccount page

--- src/pages/new-account/newAccount.page.tsx
+++ src/pages/new-account/newAccount.page.tsx
@@ -2,14 +2,15 @@ import { AppLayout } from "@/layaouts";
 import React from "react";
 import { Account, createEmptyAccount } from "./newAccount.vm";
 import {NewAccountFormComponent} from "./components/newAccount-form.component"
+import classes from "./newAccount.page.module.css"
 
 export const NewAccount : React.FC = () => {
 const [account, /*setAccount*/] = React.useState<Account>(createEmptyAccount());
 
 return (
 <AppLayout>
-    <div>
-        <h1>Cuenta Bancaria</h1>
+    <div className={classes.container}>
+        <h1 className={classes.title}>Cuenta Bancaria</h1>
         <NewAccountFormComponent account={account}>
 
         </NewAccountFormComponent>
